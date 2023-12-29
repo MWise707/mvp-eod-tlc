49c586c775d575a1340253bc0a5a3e18c9c78ece
MWise707 <markezrawiseman@gmail.com>
2023-12-29 10:42:44 -0800

    Add dynamic edit fields

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.get("/techs/:id", (req, res, next) => {
     });
 });
 
-//TODO
 app.post("/techs", (req, res, next) => {
+  console.log(req.body);
   const {
     rank,
     first_name,
@@ -75,7 +75,160 @@ app.post("/techs", (req, res, next) => {
         res.send([rank, last_name]);
       })
       .catch((err) => {
-        console.error("Error add new Tech: ", err.stack);
+        console.error("Error adding new Tech: ", err.stack);
+        next(err);
+      });
+  }
+});
+
+app.delete("/techs/:id", (req, res, next) => {
+  const { id } = req.params;
+  db.query("DELETE FROM techs WHERE tech_id = $1", [id])
+    .then((data) => {
+      console.log("Deleted succssfully:", data);
+      res.status(200).send("Successfully removed Tech");
+    })
+    .catch((err) => {
+      console.error("Error removing Tech:", err.stack);
+      next(err);
+    });
+});
+
+app.patch("/techs/:id", (req, res, next) => {
+  const { id } = req.params;
+  const {
+    rank,
+    first_name,
+    last_name,
+    position,
+    is_tlc_complete,
+    percent_complete,
+    platoon_id,
+    team_id,
+    is_officer,
+    badge_level,
+  } = req.body;
+  if (rank) {
+    db.query("UPDATE techs SET rank = $1 WHERE tech_id = $2", [rank, id])
+      .then(() => {
+        res.status(200).send(`Updated rank to ${rank}`);
+      })
+      .catch((err) => {
+        console.error("Error updating rank", err.stack);
+        next(err);
+      });
+  }
+  if (first_name) {
+    db.query("UPDATE techs SET first_name = $1 WHERE tech_id = $2", [
+      first_name,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated first_name to ${first_name}`);
+      })
+      .catch((err) => {
+        console.error("Error updating first_name", err.stack);
+        next(err);
+      });
+  }
+  if (last_name) {
+    db.query("UPDATE techs SET last_name = $1 WHERE tech_id = $2", [
+      last_name,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated last_name to ${last_name}`);
+      })
+      .catch((err) => {
+        console.error("Error updating rank", err.stack);
+        next(err);
+      });
+  }
+  if (position) {
+    db.query("UPDATE techs SET position = $1 WHERE tech_id = $2", [
+      position,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated position to ${position}`);
+      })
+      .catch((err) => {
+        console.error("Error updating position", err.stack);
+        next(err);
+      });
+  }
+  if (is_tlc_complete) {
+    db.query("UPDATE techs SET is_tlc_complete = $1 WHERE tech_id = $2", [
+      is_tlc_complete,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated is_tlc_complete to ${is_tlc_complete}`);
+      })
+      .catch((err) => {
+        console.error("Error updating is_tlc_complete", err.stack);
+        next(err);
+      });
+  }
+  if (percent_complete) {
+    db.query("UPDATE techs SET percent_complete = $1 WHERE tech_id = $2", [
+      percent_complete,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated percent_complete to ${percent_complete}`);
+      })
+      .catch((err) => {
+        console.error("Error updating percent_complete", err.stack);
+        next(err);
+      });
+  }
+  if (platoon_id) {
+    db.query("UPDATE techs SET platoon_id = $1 WHERE tech_id = $2", [
+      platoon_id,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated platoon_id to ${platoon_id}`);
+      })
+      .catch((err) => {
+        console.error("Error updating platoon_id", err.stack);
+        next(err);
+      });
+  }
+  if (team_id) {
+    db.query("UPDATE techs SET team_id = $1 WHERE tech_id = $2", [team_id, id])
+      .then(() => {
+        res.status(200).send(`Updated team_id to ${team_id}`);
+      })
+      .catch((err) => {
+        console.error("Error updating team_id", err.stack);
+        next(err);
+      });
+  }
+  if (is_officer) {
+    db.query("UPDATE techs SET is_officer = $1 WHERE tech_id = $2", [
+      is_officer,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated is_officer to ${is_officer}`);
+      })
+      .catch((err) => {
+        console.error("Error updating is_officer", err.stack);
+        next(err);
+      });
+  }
+  if (badge_level) {
+    db.query("UPDATE techs SET badge_level = $1 WHERE tech_id = $2", [
+      badge_level,
+      id,
+    ])
+      .then(() => {
+        res.status(200).send(`Updated badge_level to ${badge_level}`);
+      })
+      .catch((err) => {
+        console.error("Error updating badge_level", err.stack);
         next(err);
       });
   }
--- public/index.js
+++ public/index.js
@@ -9,11 +9,57 @@ $(document).ready(() => {
   const dashboard = $("#dashboard"); // Container for all results
   const platoons = $("#platoons");
   const addTechBtn = $("#add-tech");
+  const removeTechBtn = $("#delete-tech");
   const newTechForm = $("#new-tech-form").addClass("hidden");
+  const newTechSubmit = $("#submitNewTechForm");
+  const deleteTechBtn = $("#deleteTechBtn");
+  const selectTechForm = $("#selectTechForm").addClass("hidden");
+
+  let newTech = {
+    rank: "SGT",
+    first_name: null,
+    last_name: null,
+    position: "Unassigned",
+    is_tlc_complete: false,
+    percent_complete: 0.25,
+    platoon_id: null,
+    team_id: null,
+    is_officer: false,
+    badge_level: "BASIC",
+  };
+
+  function createTechEditForm(tech) {
+    let techEditForm = $(
+      `<form id="edit-${tech.tech_id}">${tech.rank} ${tech.last_name}</form>`
+    ).appendTo(body);
+    for (const field in tech) {
+      if (tech.hasOwnProperty(field)) {
+        let formCheckDiv = $("<div></div>")
+          .addClass("form-check")
+          .appendTo(techEditForm);
+        let checkBoxInput = $(
+          `<input type="checkbox" class="form-check-input" id="check-${field}">`
+        ).appendTo(formCheckDiv);
+        let checkBoxLabel = $(
+          `<label class="form-check-label" for="check-${field}">${field}: ${tech[field]}</label>`
+        ).appendTo(formCheckDiv);
+      }
+    }
+    //<button type="submit" class="btn btn-primary" id="editTechBtn">Edit Tech</button>
+    let editTechFieldsBtn = $(
+      `<button type="submit" class="btn btn-primary" id="edit-${tech.tech_id}selection">Edit Selected Fields</button>`
+    ).appendTo(techEditForm);
+  }
 
   function showAllTechs() {
-    $.get("/techs", (data) => {
-      console.log("All Techs Data", data);
+    return new Promise((resolve, reject) => {
+      $.get("/techs", (data) => {
+        console.log("All techs data", data);
+        resolve(data);
+      }).fail((error) => {
+        console.error("Error fetching all techs:", error);
+        reject(error);
+      });
     });
   }
 
@@ -22,7 +68,6 @@ $(document).ready(() => {
       .addClass("all-platoons")
       .appendTo(platoons);
     $.get(`/techs/${platoon_id}`, (data) => {
-      console.log(`Data from ${platoon_id} platoon`, data);
       const pltLabel = getPlatoonTitle(platoon_id);
       const platoonTitle = $(`<h1 id=${platoon_id}PLT>${pltLabel}</h1>`)
         .addClass("platoon-title")
@@ -33,9 +78,13 @@ $(document).ready(() => {
         let techDetails = $("<div></div>")
           .addClass("tech-details")
           .appendTo(`#platoon-${platoon_id}`);
-        let techTitle = $(
-          `<h2>${data[i].platoon_id}-${data[i].team_id} ${shortTitle}:</h2>`
-        ).appendTo(techDetails);
+        if (data[i].platoon_id === 4 || data[i].platoon_id === 5) {
+          let techTitle = $(`<h2>${shortTitle}:</h2>`).appendTo(techDetails);
+        } else {
+          techTitle = $(
+            `<h2>${data[i].platoon_id}-${data[i].team_id} ${shortTitle}:</h2>`
+          ).appendTo(techDetails);
+        }
         let tlcContainer = $("<div></div>")
           .addClass("tlc-container")
           .appendTo(techDetails);
@@ -50,16 +99,45 @@ $(document).ready(() => {
           .addClass("badge-container")
           .appendTo(techContainer);
         let badgeImg = $(`<img src="${badgeLink}"/>`).appendTo(badgeCont);
+        createTechEditForm(data[i]);
       }
     });
   }
 
   showPlatoon("1");
+  showPlatoon("2");
+  showPlatoon("3");
+  showPlatoon("4");
+  showPlatoon("5");
 
   function addTech(techObj) {
-    const { platoon_id } = techObj;
-    $.post("/techs", techObj).done((platoon_id) => {
-      showPlatoon(platoon_id);
+    $.post({
+      url: "/techs",
+      contentType: "application/json",
+      data: JSON.stringify(techObj),
+      success: function (data) {
+        showPlatoon(newTech.platoon_id);
+        dashboard.removeClass("hidden");
+        $(`#platoon-${newTech.platoon_id}`).removeClass("hidden");
+      },
+      error: function (error) {
+        console.error("Error adding tech:", error);
+      },
+    });
+  }
+
+  function deleteTech(techId) {
+    $.ajax({
+      url: `/techs/${techId}`,
+      type: "DELETE",
+      success: () => {
+        selectTechForm.addClass("hidden");
+        dashboard.removeClass("hidden");
+        console.log("Succss deleting tech");
+      },
+      error: (error) => {
+        console.error("Error deleting tech", error);
+      },
     });
   }
 
@@ -67,14 +145,39 @@ $(document).ready(() => {
     if (platoon_id === "1") return "1st Platoon";
     if (platoon_id === "2") return "2nd Platoon";
     if (platoon_id === "3") return "3rd Platoon";
-    if (platoon_id === "HQ") return "HQ Platoon";
-    if (platoon_id === null) return "UNASSIGNED";
+    if (platoon_id === "4") return "HQ Platoon";
+    if (platoon_id === null || platoon_id === "5") return "UNASSIGNED";
+  }
+
+  function getPlatoonId(pltTitle) {
+    if (pltTitle === "1st Platoon") return 1;
+    if (pltTitle === "2nd Platoon") return 2;
+    if (pltTitle === "3rd Platoon") return 3;
+    if (pltTitle === "HQ Platoon") return 4;
+    if (pltTitle === "UNASSIGNED") return 5;
+  }
+
+  function getTeamId(teamSelected) {
+    if (teamSelected === "1 (1-1, 2-1, or 3-1)") return 1;
+    if (teamSelected === "2 (1-2, 2-2, or 3-2)") return 2;
+    if (teamSelected === "3 (1-3, 2-3, or 3-3)") return 3;
+    if (teamSelected === "Not on a team") return null;
+  }
+
+  function getRank(rankSelected) {
+    return rankSelected.slice(-3);
   }
 
   function getPostionTitle(position) {
     if (position === "Team Leader") return "TL";
     if (position === "Senior Team Member") return "TM";
     if (position === "Team Member") return "TM";
+    if (position === "Platoon Leader") return "PL";
+    if (position === "Platoon Sergeant") return "PSG";
+    if (position === "Operations NCO") return "OPS NCO";
+    if (position === "Company Commander") return "CDR";
+    if (position === "First Sergeant") return "1SG";
+    if (position === "Unassigned") return "Unassigned";
   }
 
   function getBadgePic(badge_level) {
@@ -96,15 +199,80 @@ $(document).ready(() => {
     }
   }
 
-  $("body").on("click", "#add-tech", (e) => {
-    console.log("Add-Tech button clicked");
-    dashboard.toggleClass("hidden");
-    newTechForm.toggleClass("hidden");
+  function processNewTechForm() {
+    newTech.rank = getRank($("#rankSelected option:selected").text());
+    newTech.first_name = $("#inputFirstName").val();
+    newTech.last_name = $("#inputLastName").val();
+    newTech.position = $("#positionSelected option:selected").text();
+    $("#tlcCheck").is(":checked")
+      ? (newTech.is_tlc_complete = true)
+      : (newTech.is_tlc_complete = false);
+    newTech.is_tlc_complete
+      ? (newTech.percent_complete = 1)
+      : (newTech.percent_complete = Number($("#inputTLCpercent").val()));
+    newTech.platoon_id = getPlatoonId(
+      $("#platoon-selected option:selected").text()
+    );
+    newTech.team_id = getTeamId($("#team-selected option:selected").text());
+    newTech.is_officer =
+      newTech.rank === "2LT" ||
+      newTech.rank === "1LT" ||
+      newTech.rank === "CPT";
+    newTech.badge_level = $("#badge-selected option:selected").text();
+  }
+
+  function populateTechSelection() {
+    showAllTechs()
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          let techOption = $(
+            `<option id="${data[i].tech_id}">${data[i].rank} ${
+              data[i].first_name
+            } ${data[i].last_name}: ${getPlatoonTitle(
+              String(data[i].platoon_id)
+            )}</option>`
+          ).appendTo("#tech-selected");
+        }
+      })
+      .catch((error) => {
+        console.error("Error populating tech selection:", error);
+      });
+  }
+  populateTechSelection();
+
+  addTechBtn.on("click", () => {
+    // $("#platoon-1").addClass("hidden");
+    // $("#platoon-2").addClass("hidden");
+    // $("#platoon-3").addClass("hidden");
+    // $("#platoon-4").addClass("hidden");
+    // $("#platoon-5").addClass("hidden");
+    dashboard.addClass("hidden");
+    newTechForm.removeClass("hidden");
   });
 
-  // addTechBtn.on("click", (e) => {
-  //   console.log("Add-Tech button clicked");
-  // });
+  removeTechBtn.on("click", () => {
+    console.log("Delete tech btn clicked");
+    // $("#platoon-1").addClass("hidden");
+    // $("#platoon-2").addClass("hidden");
+    // $("#platoon-3").addClass("hidden");
+    // $("#platoon-4").addClass("hidden");
+    // $("#platoon-5").addClass("hidden");
+    dashboard.addClass("hidden");
+    selectTechForm.removeClass("hidden");
+  });
 
-  // ==============
+  newTechSubmit.on("click", (e) => {
+    e.preventDefault();
+    processNewTechForm();
+    console.log(newTech);
+    newTechForm.addClass("hidden");
+    addTech(newTech);
+  });
+
+  deleteTechBtn.on("click", (e) => {
+    // e.preventDefault();
+    const techId = $("#tech-selected option:selected").attr("id");
+    console.log("Selected Tech ID:", techId);
+    deleteTech(techId);
+  });
 });
